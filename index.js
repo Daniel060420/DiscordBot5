d08e660f17916c89e6d2f0ee6fbc32e29bb8caa8
Daniel <140344449+Daniel060420@users.noreply.github.com>
2023-08-31 06:22:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 const { Client, Intents } = require('discord.js')
-const { CommandManager } = require('discord-bot-create')
+const { CommandManager, EventManager } = require('discord-bot-create')
 const client = new Discord.Client({
     intents: ['MessageContent', 'GuildMessages', 'Guilds']
 });
@@ -9,8 +9,11 @@ client.on('ready', () => {
     console.log('El bot se ha conectado')
 });
 const comandos = new CommandManager()
+const event = new EventManager()
 comandos.definirPrefijoPorDefecto('>>')
 comandos.cargarComandosDesdeDirectorio('./commands')
+event.CargarEventosDesdeDirectorio(client, './event')
+
 client.on('messageCreate', (message) => {
     if(!message.content.startsWith(comandos.prefijoPorDefecto)) return;
     comandos.ejecutarComando(client, message)
