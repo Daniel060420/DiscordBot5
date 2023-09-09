43a344f129683469c475a2712f561bf2dc57f591
Daniel <140344449+Daniel060420@users.noreply.github.com>
2023-09-09 06:03:42 -0600

    Update messageCreate.js

--- event/messageCreate.js
+++ event/messageCreate.js
@@ -1,5 +1,9 @@
+const comandos = require('../index.js')
 module.exports = {
-  nombre: "messageCreate",
-  ejecutar: async (client, message) => {
-    console.log('Se ha enviado el mensaje: ', message.content)
-}}
+    nombre: 'messageCreate',
+    ejecutar: async (client, message) => {
+        console.log('Se ha enviado el mensaje: ', message.content)
+        if(!message.content.startsWith(comandos.prefijoPorDefecto)) return;
+        comandos.ejecutarComando(client, message)
+    }
+}
